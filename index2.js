f024ab09a1ff89e1cd112d0167a7e64c54ca5949
wommy <tom@technomad.media>
2021-10-11 17:26:41 -0400

    pushit

--- index2.js
+++ index2.js
@@ -2,11 +2,17 @@
 const fs = require('fs')
 
 const data = fs.readFileSync('./copypaste.txt', 'utf-8').split('\n')
-// console.log(data)
+let rows = []
 
-for ( ea of data ){
-	console.log(ea)
+for ( let i = 0; i < data.length; i+=5 ){
+	rows.push([data[i],data[i+1],data[i+2],data[i+3],data[i+4]])
 }
+
+console.log(rows)
+
+// for ( ea of data ){
+// 	console.log(ea)
+// }
 
 // const rows = [ data[0], data[1], data[2], data[3], data[4] ]
 // const cols = 5
