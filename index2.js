be17f5d950d0b42dbcf5e81755f8aa5a1eefb5d2
wommy <tom@technomad.media>
2021-10-11 19:08:09 -0400

    pushit

--- index2.js
+++ index2.js
@@ -3,11 +3,14 @@ const fs = require('fs')
 
 const data = fs.readFileSync('./copypaste.txt', 'utf-8').split('\n')
 let rows = []
+let total = [0]
 
 for ( let i = 5; i < data.length; i+=5 ){
+
 	let chapter = data[i+2].split(':').shift()
 	let week = +chapter.split(' ').pop()
 	let assignment = data[i+1]
+
 	rows.push([
 		(week < 24 && 5 || week < 29 && 6 || week < 34 && 7 || week < 39 && 8 || '-').toString(),
 		'Karch',
@@ -15,9 +18,24 @@ for ( let i = 5; i < data.length; i+=5 ){
 		assignment,
 		data[i+3].split(' ', 3).join(' '),
 		data[i+4],
-		()
+		(
+			assignment.split(' ').shift() == 'PrepU' && '1'
+			|| assignment == 'Pre-Lecture Quiz' && '0.5'
+			|| assignment === '-' && '0'
+			|| '.25'
+		),
+		(
+			assignment.split(' ').shift() == 'PrepU' && total.push(1)
+			|| assignment == 'Pre-Lecture Quiz' && total.push(0.5)
+			|| assignment === '-' && total.push(0)
+			|| total.push(0.25)
+		) && total.reduce( (p,c) => p + c),
+			
 	])
 }
+
+let headers = ['week', 'book', 'chapter', 'assignment', 'date', 'grade', 'points', 'total']
+rows.unshift(headers)
 
 console.log(rows)
 
