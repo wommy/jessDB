2bbe25aa33afecdcbce5dece25d60ff5cbebecb7
wommy <tom@technomad.media>
2021-10-11 17:14:41 -0400

    pushit

--- index2.js
+++ index2.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 const fs = require('fs')
 
-const data = fs.readFileSync('./copypaste.txt', 'utf-8')
+const data = fs.readFileSync('./copypaste.txt', 'utf-8').split('\n')
+// console.log(data)
 
-console.log(data)
+for ( ea of data ){
+	console.log(ea)
+}
+
+// const rows = [ data[0], data[1], data[2], data[3], data[4] ]
+// const cols = 5
+
+// console.log(rows)
